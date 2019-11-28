69c2d342e3a1edb6ace00f8ff20a02d648fa914e
BryantCancino <bryantcancino@gmail.com>
2019-11-28 11:45:30 -0600

    pld_nat-init

---
+++ src/test/java/io/PLDNaturalesSimulacion/client/api/ApiTest.java
@@ -0,0 +1,58 @@
+package io.PLDNaturalesSimulacion.client.api;
+
+import io.PLDNaturalesSimulacion.client.ApiException;
+import io.PLDNaturalesSimulacion.client.model.Peticion;
+import io.PLDNaturalesSimulacion.client.model.Respuesta;
+import okhttp3.OkHttpClient;
+import io.PLDNaturalesSimulacion.client.ApiClient;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ApiTest {
+	
+	private Logger logger = LoggerFactory.getLogger(ApiTest.class.getName());
+	private final PldNaturalesSimulacionApi api = new PldNaturalesSimulacionApi();    
+	private ApiClient apiClient = null;	
+    
+	@Before()
+	public void setUp() {
+		this.apiClient = api.getApiClient();
+		this.apiClient.setBasePath("the_url");
+    	OkHttpClient insecureClient = ApiClient.getClientNoSSLVerification();
+    	OkHttpClient okHttpClient = insecureClient.newBuilder()
+    			.readTimeout(60, TimeUnit.SECONDS)
+    			.build();
+    	apiClient.setHttpClient(okHttpClient);	
+	}    
+    
+    @Test
+    public void pldTest() throws ApiException {
+        String xApiKey = "your_api_key";
+        Peticion persona = new Peticion();
+        
+        persona.setFolioOtorgante("000008");
+        persona.setTipoDocumento("01");
+        persona.setNumeroDocumento("01");
+        persona.setNombre("Maria");
+        persona.setSegundoNombre(null);
+        persona.setApellidoPaterno("Sanchez");
+        persona.setApellidoMaterno("Mathews");
+        
+		try {
+			Respuesta response = api.pld(xApiKey, persona);
+	        Assert.assertTrue(response != null);
+	        if(response != null) {
+	        	logger.info(response.toString());
+	        }
+		} catch (ApiException e) {
+			logger.info(e.getResponseBody());
+		}
+    }
+    
+}
